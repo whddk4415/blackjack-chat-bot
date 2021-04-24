d7108d7e64726c94cda49eafac0e9b5d17fbc6fe
이종아 <whddk4415@gmail.com>
2021-04-24 16:44:22 +0000

    feat: :sparkles: '날씨 어때?' 메세지 라우터 추가

--- routes/callback.js
+++ routes/callback.js
@@ -10,6 +10,46 @@ router.post('/', async (req, res, next) => {
   const user = await detail(react_user_id);
 
   switch (value) {
+    case 'callWhatIsTheWeatherIntroMessage':
+      messages.sendMessage({
+        conversationId,
+        text: '날씨 어때?',
+        blocks: [
+          {
+            type: 'header',
+            text: '날씨 어때?',
+            style: 'blue',
+          },
+          {
+            type: 'text',
+            text:
+              '날씨 어때? 날씨 어때? 날씨 어때? 날씨 어때? 날씨 어때? 날씨 어때?',
+            markdown: true,
+          },
+          {
+            type: 'action',
+            elements: [
+              {
+                type: 'button',
+                text: '지금 어때?',
+                style: 'primary',
+                action_type: 'submit_action',
+                action_name: 'callWhatIsTheWeatherNowMessage',
+                value: 'callWhatIsTheWeatherNowMessage',
+              },
+              {
+                type: 'button',
+                text: '오늘 어때?',
+                style: 'danger',
+                action_type: 'submit_action',
+                action_name: 'callWhatIsTheWeatherTodayMessage',
+                value: 'callWhatIsTheWeatherTodayMessage',
+              },
+            ],
+          },
+        ],
+      });
+      break;
     default:
   }
   res.json({ result: true });
