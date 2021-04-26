20cf074190734f1bcd29ee0e902b5d8f8a34582a
이종아 <whddk4415@gmail.com>
2021-04-26 17:01:32 +0000

    feat: :sparkles: 매일 알람 메시지를 보내는 함수 생성

--- common/utils/index.js
+++ common/utils/index.js
@@ -1,25 +1,17 @@
 const { getUserList } = require('../../libs/kakaoWork/users');
-const { detail, create } = require('../../controller/users');
+const { list, detail, create, update } = require('../../controller/users');
 const {
   detail: weatherDetail,
   create: weatherCreate,
 } = require('../../controller/weathers');
 const dateFormat = require('dateformat');
-const { getWeather, getCityData } = require('../../libs/weather');
+const { getWeather, getCityData, getClothes } = require('../../libs/weather');
 const schedule = require('node-schedule');
-
-exports.initUsers = async (userId, conversationId) => {
-  if (!(await detail({ user_id: userId }))) {
-    create({
-      user_id: userId,
-      rain_alarm: false,
-      dust_alarm: false,
-      daily_alarm: false,
-      city: null,
-      conversation_id: conversationId,
-    });
-  }
-};
+const {
+  sendDailyAlarmMessage,
+  sendRainAlarmMessage,
+  sendDustAlarmMessage,
+} = require('../../libs/actions/message');
 
 const initWeather = async () => {
   const currentTime = dateFormat(new Date(), 'yyyy-mm-dd HH');
@@ -27,7 +19,6 @@ const initWeather = async () => {
   for (const { kor: city } of cityData) {
     if (!(await weatherDetail(currentTime, city))) {
       const weather = await getWeather(city);
-      console.log(city, weather);
       weather.time = currentTime;
       weather.city = city;
       weatherCreate(weather);
@@ -41,4 +32,52 @@ exports.getWeatherPerHour = async () => {
   const job = schedule.scheduleJob({ minute: 0 }, async () => {
     initWeather();
   });
+};
+
+exports.sendDailyAlarmMessageEveryDay = async () => {
+  const job = schedule.scheduleJob(
+    { hour: 22, minute: 0, second: 15 },
+    async () => {
+      const users = await list({ daily_alarm: true });
+      const currentTime = dateFormat(new Date(), 'yyyy-mm-dd HH');
+      for (const { city, conversation_id } of users) {
+        sendDailyAlarmMessage(
+          conversation_id,
+          await weatherDetail(currentTime, city),
+        );
+      }
+    },
+  );
+};
+
+exports.sendRainAlarmMessageEveryDay = async () => {
+  const job = schedule.scheduleJob(
+    { hour: 22, minute: 0, second: 15 },
+    async () => {
+      const users = await list({ rain_alarm: true });
+      const currentTime = dateFormat(new Date(), 'yyyy-mm-dd HH');
+      for (const { city, conversation_id } of users) {
+        sendRainAlarmMessage(
+          conversation_id,
+          await weatherDetail(currentTime, city),
+        );
+      }
+    },
+  );
+};
+
+exports.sendDustAlarmMessageEveryDay = async () => {
+  const job = schedule.scheduleJob(
+    { hour: 22, minute: 0, second: 15 },
+    async () => {
+      const users = await list({ dust_alarm: true });
+      const currentTime = dateFormat(new Date(), 'yyyy-mm-dd HH');
+      for (const { city, conversation_id } of users) {
+        sendDustAlarmMessage(
+          conversation_id,
+          await weatherDetail(currentTime, city),
+        );
+      }
+    },
+  );
 };
