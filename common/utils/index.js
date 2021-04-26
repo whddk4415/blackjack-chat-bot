4244352dc716fe0a7570a1247dac3c43f8335511
이종아 <whddk4415@gmail.com>
2021-04-26 15:34:07 +0000

    feat: :sparkles: 서버가 시작했을 때 현재 시간에 해당하는 날씨를 db에 초기화하는 함수 추가

--- common/utils/index.js
+++ common/utils/index.js
@@ -1,5 +1,11 @@
 const { getUserList } = require('../../libs/kakaoWork/users');
 const { detail, create } = require('../../controller/users');
+const {
+  detail: weatherDetail,
+  create: weatherCreate,
+} = require('../../controller/weathers');
+const dateFormat = require('dateformat');
+const { getWeather, getCityData } = require('../../libs/weather');
 
 exports.initUsers = async (userId, conversationId) => {
   if (!(await detail({ user_id: userId }))) {
@@ -14,3 +20,18 @@ exports.initUsers = async (userId, conversationId) => {
   }
 };
 
+const initWeather = async () => {
+  const currentTime = dateFormat(new Date(), 'yyyy-mm-dd HH');
+  const cityData = getCityData();
+  for (const { kor: city } of cityData) {
+    if (!(await weatherDetail(currentTime, city))) {
+      const weather = await getWeather(city);
+      console.log(city, weather);
+      weather.time = currentTime;
+      weather.city = city;
+      weatherCreate(weather);
+    }
+  }
+};
+
+exports.initWeather = initWeather;
