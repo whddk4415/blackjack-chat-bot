4834288c96a38d9c352acee6c65cfb45c06dad59
이종아 <whddk4415@gmail.com>
2021-04-26 11:00:16 +0000

    chore: :fire: app.js에서 initUser() 제거

--- app.js
+++ app.js
@@ -4,25 +4,22 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-const {initUsers} = require('./common/utils')
 const index = require('./routes/index');
-const request = require('./routes/request')
-const callback = require('./routes/callback')
+const request = require('./routes/request');
+const callback = require('./routes/callback');
 
 const app = express();
 
 // mongoDB connect
 mongoose.connect('mongodb://localhost/chatbot', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 const db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function () {
-    console.log("Connected to mongod server")
+  console.log('Connected to mongod server');
 });
-
-initUsers()
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -33,14 +30,14 @@ app.use('/', index);
 app.use('/request', request);
 app.use('/callback', callback);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -50,7 +47,8 @@ app.use(function(err, req, res, next) {
   res.json({ err });
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('ChatBot app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () =>
+  console.log('ChatBot app listening on port 3000!'),
+);
 
 module.exports = app;
-
